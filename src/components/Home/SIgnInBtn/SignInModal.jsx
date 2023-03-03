ddd91811b0b4444633be2e83355fceb8a8da5539
Jasper-Duong <duongngochung13@gmail.com>
2023-03-03 10:11:49 +0700

    Add Sign In & Sign Up Modal

---
+++ src/components/Home/SIgnInBtn/SignInModal.jsx
@@ -0,0 +1,70 @@
+import { Form, Button, Input } from "antd";
+import { LockOutlined, UserOutlined } from "@ant-design/icons";
+import React from "react";
+import styled from "styled-components";
+
+export default function SignInModal() {
+  const onFinish = (values) => {
+    console.log("Success:", values);
+  };
+  return (
+    <Styled>
+      <Form
+        name="normal_login"
+        className="login-form"
+        initialValues={{
+          remember: true,
+        }}
+        onFinish={onFinish}
+      >
+        <Form.Item
+          name="username"
+          rules={[
+            {
+              required: true,
+              message: "Please input your Username!",
+            },
+          ]}
+        >
+          <Input
+            prefix={<UserOutlined className="site-form-item-icon" />}
+            placeholder="Username"
+          />
+        </Form.Item>
+        <Form.Item
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: "Please input your Password!",
+            },
+          ]}
+        >
+          <Input.Password
+            prefix={<LockOutlined className="site-form-item-icon" />}
+            type="password"
+            placeholder="Password"
+          />
+        </Form.Item>
+        <Form.Item className="login-form-btn-item">
+          <Button
+            type="primary"
+            htmlType="submit"
+            className="login-form-button"
+          >
+            Log in
+          </Button>
+        </Form.Item>
+      </Form>
+    </Styled>
+  );
+}
+
+const Styled = styled.div`
+  .login-form {
+    max-width: 350px;
+  }
+  .login-form-button {
+    width: 100%;
+  }
+`;
