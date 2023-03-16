0e20a82f0470a550eebb0d3f7b382bd92bebc7f1
Jasper-Duong <duongngochung13@gmail.com>
2023-03-16 22:34:14 +0700

    Add AboutThisJob

--- src/pages/JobDetail/AboutThisJob/AboutThisJob.jsx
+++ src/pages/JobDetail/AboutThisJob/AboutThisJob.jsx
@@ -1,5 +1,108 @@
 import React from "react";
+import styled from "styled-components";
 
 export default function AboutThisJob() {
-  return <div>AboutThisJob</div>;
+  return (
+    <Styled>
+      <h4>About This Job</h4>
+      <div className="description-content">
+        <p>
+          Are you looking for a{" "}
+          <strong>flat modern minimalist business logo</strong>?&nbsp;
+        </p>
+        <p>You are at the Perfect Place.</p>
+        <p>
+          <br />
+        </p>
+        <p>
+          Designs_storm is the world of the creative innovative professional
+          flat modern minimalist logo.
+        </p>
+        <p>
+          <br />
+        </p>
+        <p>
+          <strong>"Design is the silent ambassador of your brand"</strong>
+          &nbsp;That's why we are here to give a creative and unique design,
+          that everything gives your business a unique identity, in a creative
+          way.&nbsp;
+        </p>
+        <p>
+          <br />
+        </p>
+        <p>
+          <strong>Why us?&nbsp;</strong>
+        </p>
+        <p>
+          <br />
+        </p>
+        <ul>
+          <li>Logo concept from scratch.</li>
+          <li>Copyrights Will Be With The Customer.</li>
+          <li>UNLIMITED revisions.</li>
+          <li>Assured prompt delivery.</li>
+          <li>
+            Reliable and Premium support throughout the order and after-sales as
+            well.
+          </li>
+          <li>
+            Flexible Source Formats:-{" "}
+            <strong>.AI | .EPS | .SVG | .PDF | .PSD | .JPEG | .PNG</strong>
+          </li>
+        </ul>
+        <p>
+          <br />
+        </p>
+        <p>
+          We have an experienced Certified and Award-winning Designers team
+          working over the last 8 years in logo designing and brand identity.
+        </p>
+        <p>
+          <br />
+        </p>
+        <p>
+          <strong>Check out my live portfolio and be assured.</strong>
+        </p>
+        <p>
+          <u>Portfolio: https://flic.kr/ps/3QYciN</u>
+        </p>
+        <p>
+          <br />
+        </p>
+        <p>
+          <strong>Go ahead and ORDER NOW!</strong>
+        </p>
+        <p>
+          <br />
+        </p>
+        <p>
+          <strong>Our Expertise :</strong>
+        </p>
+        <p>
+          Minimalist | Minimal | Flat | Modern | Professional | Text | Vintage |
+          Hand drawn | Feminine | Signature | Custom Logo Design |{" "}
+          <strong>NFT Logo design</strong>
+        </p>
+        <p>
+          <br />
+        </p>
+        <p>
+          <br />
+        </p>
+        <p>
+          <strong>Note:</strong>
+        </p>
+        <p>
+          - Our Display/portfolio Logo Falls Under Standard Or Premium Package.
+          Kindly Order An Appropriate Package According To Your Quality
+          Requirements.
+        </p>
+      </div>
+    </Styled>
+  );
 }
+const Styled = styled.div`
+  h4 {
+    margin: 4rem 0 2rem;
+  }
+`;
