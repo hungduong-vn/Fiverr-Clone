c10216b99c7f12fe0f552f0adf5218831913e172
Jasper-Duong <duongngochung13@gmail.com>
2023-03-16 22:55:17 +0700

    Replace JobNavTabs -> JobNavScroll (react-scroll)

--- src/pages/JobDetail/JobDetailNav/JobDetailNav.jsx
+++ src/pages/JobDetail/JobDetailNav/JobDetailNav.jsx
@@ -1,46 +1,48 @@
-import React, { useRef } from "react";
+import { Divider } from "antd";
+import React from "react";
+import { useMediaQuery } from "react-responsive";
 import styled from "styled-components";
-import JobNavTabs from "./JobNavTabs";
+import { breakpoints } from "../../../constants/common";
+import JobNavScroll from "../JobNavScroll/JobNavScroll";
 import JobsDetailActions from "./JobsDetailActions";
 
 export default function JobDetailNav({ sections }) {
-  const jobNavRef = useRef();
+  const isScreenMedium = useMediaQuery({
+    query: `(min-width : ${breakpoints.medium}px)`,
+  });
   return (
-    <Styled ref={jobNavRef}>
-      <div className="jobNav-container">
-        <div className="jobNav-content">
-          {jobNavRef && (
-            <JobNavTabs jobNavRef={jobNavRef} sections={sections} />
-          )}
-          <JobsDetailActions />
-        </div>
+    <Styled>
+      <div className="max-width-container">
+        <header>
+          <JobNavScroll sections={sections} />
+        </header>
+        {isScreenMedium && <JobsDetailActions />}
       </div>
     </Styled>
   );
 }
 
 const Styled = styled.div`
-  overflow: hidden;
+  position: sticky;
+  z-index: 10;
+  top: 0;
+  width: 100%;
+  background-color: #fff;
   border-bottom: 1px solid #e4e5e7;
-  .jobNav-container {
-    padding: 0 2rem;
-  }
-  .jobNav-content {
+  .max-width-container {
     display: flex;
     justify-content: space-between;
-    align-items: center;
+    flex-wrap: nowrap;
+    white-space: nowrap;
+    overflow: scroll;
+    padding-bottom: 0;
   }
-  &.sticky {
-    z-index: 1;
-    background-color: #fff;
-    position: fixed;
-    top: 0;
-    width: 100%;
-  }
-  @media only screen and (min-width: 1160px) {
-    .jobNav-container {
+  @media only screen and (max-width: 599px) {
+    .max-width-container {
+      padding: 0;
+    }
+    header {
       margin: 0 auto;
-      max-width: 1400px;
     }
   }
 `;
