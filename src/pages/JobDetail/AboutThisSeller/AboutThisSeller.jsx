8184afe3a1f1d3699ac4667f73cdc6a83fbd57bd
Jasper-Duong <duongngochung13@gmail.com>
2023-03-16 22:54:10 +0700

    Finished AboutThisSeller

--- src/pages/JobDetail/AboutThisSeller/AboutThisSeller.jsx
+++ src/pages/JobDetail/AboutThisSeller/AboutThisSeller.jsx
@@ -1,7 +1,40 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+import { defSellerInfo } from "../../UserProfile/data";
+import AboutSellerTop from "./AboutSellerTop/AboutSellerTop";
+import AboutThisSellerBody from "./AboutThisSellerBody/AboutThisSellerBody";
 
-export default function AboutThisSeller() {
+export default function AboutThisSeller({ userId }) {
+  const seller = defSellerInfo;
+  const {
+    name,
+    id,
+    birthday,
+    created_at,
+    nationality,
+    languages,
+    avatar,
+    title,
+    self_intro,
+  } = seller;
+  const sellerTop = { avatar, name, id, title };
+  const sellerBody = {
+    birthday,
+    created_at,
+    nationality,
+    languages,
+    self_intro,
+  };
   return (
-    <div>AboutThisSeller</div>
-  )
+    <Styled>
+      <h4>About The Seller</h4>
+      <AboutSellerTop sellerTop={sellerTop} />
+      <AboutThisSellerBody sellerBody={sellerBody} />
+    </Styled>
+  );
 }
+const Styled = styled.div`
+  h4 {
+    margin: 4rem 0 2rem;
+  }
+`;
