890716e2fa039881f98497f0a39b48432c3dd978
Jasper-Duong <duongngochung13@gmail.com>
2023-03-19 15:12:03 +0700

    Add JobReviews

--- src/pages/JobDetail/JobReviews/JobReviews.jsx
+++ src/pages/JobDetail/JobReviews/JobReviews.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import { Divider } from "antd";
+import React from "react";
+import styled from "styled-components";
+import { calcStar } from "../../../utils/common";
+import { reviews } from "./data";
+import JobReview from "./JobReview/JobReview";
+import JobReviewsStats from "./JobReviewsStats/JobReviewsStats";
 
 export default function JobReviews() {
+  const totalReviews = reviews.length;
+  let totalStar = calcStar(reviews.map((review) => review.stars));
   return (
-    <div>JobReviews</div>
-  )
+    <Styled>
+      <h4>Reviews</h4>
+      <JobReviewsStats totalReviews={totalReviews} totalStar={totalStar} />
+      {reviews.map((review) => (
+        <React.Fragment key={review.id}>
+          <Divider />
+          <JobReview review={review} />
+        </React.Fragment>
+      ))}
+    </Styled>
+  );
 }
+const Styled = styled.div`
+  h4 {
+    margin: 2rem 0;
+  }
+`;
