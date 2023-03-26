b438e07799831c0fe6dad67ba490ebb9bb9f10c6
Jasper-Duong <duongngochung13@gmail.com>
2023-03-26 23:48:43 +0700

    EditProfile call getUserAuth

--- src/services/user.js
+++ src/services/user.js
@@ -4,8 +4,15 @@ async function getUserByName(name) {
   return request({ method: "GET", url: `users/get-user-by-name/${name}` });
 }
 
+async function getUserByNameAuthApi(name) {
+  return request({
+    url: `users/get-user-by-name-author/${name}`,
+    method: "GET",
+  });
+}
+
 async function updateUser(data) {
   return request({ method: "PUT", url: `users/update-user`, data });
 }
 
-export { getUserByName, updateUser };
+export { getUserByName, getUserByNameAuthApi, updateUser };
