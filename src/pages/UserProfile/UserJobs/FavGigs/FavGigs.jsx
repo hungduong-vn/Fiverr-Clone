c94ea6d3704ce6644355ca7f0b2c1043dacb8b3e
Jasper-Duong <duongngochung13@gmail.com>
2023-03-26 23:37:24 +0700

    AddToWishList dispatch changeFavJobs

--- src/pages/UserProfile/UserJobs/FavGigs/FavGigs.jsx
+++ src/pages/UserProfile/UserJobs/FavGigs/FavGigs.jsx
@@ -1,11 +1,37 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import EmptyFavJobIcon from "../../../../components/Svg/EmptyFavJobIcon";
+import { useAsync } from "../../../../hooks/useAsync";
+import { getLovedJobByUserIdApi } from "../../../../services/job";
 import UserJobsSection from "../UserJobsSection/UserJobsSection";
 
 export default function FavGigs() {
   const title = "My Favorite Gigs";
+  const {
+    userInfo: { id },
+  } = useSelector((state) => state.userReducer);
+  const { favJobs } = useSelector((state) => state.jobReducer);
+  const isFavJobsChanged = favJobs.isChanged;
+  const { state: lovedJobs } = useAsync({
+    service: () => getLovedJobByUserIdApi(id),
+    deps: [isFavJobsChanged],
+  });
+  // console.log({ lovedJobs });
+
+  const emptyState = {
+    Icon: EmptyFavJobIcon,
+    heading: "Need some inspiration?",
+    buttonText: "Explore more",
+  };
   return (
     <div>
-      <UserJobsSection sectionTitle={title} />
+      {lovedJobs && (
+        <UserJobsSection
+          listData={lovedJobs}
+          sectionTitle={title}
+          emptyState={emptyState}
+        />
+      )}
     </div>
   );
 }
--- src/pages/UserProfile/UserJobs/SellingGigs/SellingGigs.jsx
+++ src/pages/UserProfile/UserJobs/SellingGigs/SellingGigs.jsx
@@ -1,13 +1,28 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import EmptySellingJobIcon from "../../../../components/Svg/EmptySellingJobIcon";
+import { useAsync } from "../../../../hooks/useAsync";
+import { getJobsByUserApi } from "../../../../services/job";
 import { jobs } from "../../../JobList/JobsGrid/data";
 import UserJobsSection from "../UserJobsSection/UserJobsSection";
 
 export default function SellingGigs({ sectionTitle }) {
+  const { userName } = useParams();
+  const { state: sellingJobs } = useAsync({
+    service: () => getJobsByUserApi({ userName }),
+  });
+  const jobList = sellingJobs || jobs.slice(2, 3);
+  const emptyState = {
+    Icon: EmptySellingJobIcon,
+    heading: "Ready to earn on your own terms",
+    buttonText: "Become a seller",
+  };
   return (
     <div>
       <UserJobsSection
-        listData={jobs.slice(2, 3)}
+        listData={jobList}
         sectionTitle={sectionTitle || "My Selling Gigs"}
+        emptyState={emptyState}
       />
     </div>
   );
