b1900b5e5a44ff8b06ec316247736f6aa549ad90
Jasper-Duong <duongngochung13@gmail.com>
2023-03-27 16:51:05 +0700

    Map to domain saltair.site

--- src/constants/common.js
+++ src/constants/common.js
@@ -8,4 +8,4 @@ export const breakpoints = {
 
 export const userRole = { admin: "admin", seller: "seller", user: "user" };
 
-export const BASE_URL = "https://103.176.111.241:8080/api/";
+export const BASE_URL = "https://saltair.site/api/";
